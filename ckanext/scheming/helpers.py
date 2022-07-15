e83f944511194d4d23b010fb84ee74470e2da45a
qucoso <qucoso@gmail.com>
2022-07-15 08:25:10 +0200

    added the get_common_map helpers function

--- ckanext/scheming/helpers.py
+++ ckanext/scheming/helpers.py
@@ -589,3 +589,12 @@ def count(data):
     for item in data.values():
         i += count(item)
     return max(i, 1)
+
+@helper   
+def get_common_map_config():
+    '''
+        Returns a dict with all configuration options related to the common
+        base map (ie those starting with 'ckanext.spatial.common_map.')
+    '''
+    namespace = 'ckanext.spatial.common_map.'
+    return dict([(k.replace(namespace, ''), v) for k, v in config.items() if k.startswith(namespace)])
